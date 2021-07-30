ee73edc244233e729179841eb39fc8ff4174f328
CupOJoe-hub <66897604+CupOJoe-hub@users.noreply.github.com>
2021-07-30 14:53:20 +1200

    Add files via upload

--- PlayerStats.cs
+++ PlayerStats.cs
@@ -1,115 +1,106 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-//This Script Handles the health, defence, stamina and general statistics of the player
+
+//This Script Collects Data From The Player Such As Player Name, Game Difficulty, And Character Customisation
 public class PlayerStats : MonoBehaviour
 {
-    //These variables are the stats of the player
-    [Header("Stats")]
-    public float maxHealth;
-    private float currentHealth;
-    public Image healthBar;
-
-    public int defence;
-    public int stamina;
-    public int attack;
-
-    public int maxStats;
-    public int statsStart;
-    public int statsLeft;
-
-    public Text defenceText;
-    public Text staminaText;
-    public Text attackText;
-
-    public int coins;
-
-    public int difficulty;
-    public int maxDifficulty;
-    public bool hasDifficultyInput;
-
-    //Control variables
-    [Header("Control Variables")]
-    public float healthRegen;
-    public float healthRegenWaitTime;
-
-    //UI & Input
-    [Header("UI & Input")]
-    public InputField input;
+    //Variables That Store And Collect Name Input
     public string name;
-    private bool isOpen;
-    private bool hasInput;
+    public bool hasInputedName;
+    public InputField nameInput;
+
+    //Variables That Store And Collect Game Difficulty Input
+    public int difficulty;
+    public bool hasInputedDifficulty;
     public InputField difficultyInput;
 
-    private void Start() {
-        input.gameObject.SetActive(true);
+    //Variables That Change The Players Damage, Health And Defence
+    public int attack;
+    public int health;
+    public int defence;
 
-        DontDestroyOnLoad(gameObject);
-    }
+    //Initialises Game Based On If They Have Played Or Not
+    public InputField hasPlayedInput;
+    public bool hasPlayed;
+    public string[] acceptedHasPlayedStringsTrue;
+    public string[] acceptedHasPlayedStringsFalse;
 
+    public string[] wierdInputStrings;
+
+    //In The Update Function I Will Collect Variables Data
     private void Update() {
-        if(input.text != null) {
-            hasInput = true;
-        }else {
-            Debug.Log("Input Text");
+        if(hasInputedName == false) { 
+            if(nameInput.text != null && Input.GetKeyDown(KeyCode.Return)) {
+                name = nameInput.text;
+                hasInputedName = true;
+            }
         }
 
-        if(difficultyInput.text != null) {
-            hasDifficultyInput = true;
-        }else {
-            Debug.Log("Input Text");
-        }
-
-        //checks if there is text in the input box and that the enter key has been pressed
-        if(hasInput && Input.GetKeyDown(KeyCode.Return)) {
-            name = input.text;
-        }
-
-        if(hasDifficultyInput && Input.GetKeyDown(KeyCode.Return)) {
-            if(Int32.TryParse(difficultyInput.text, out difficulty) == true) {
-                difficulty = Int32.Parse(difficultyInput.text);
-                if(difficulty > maxDifficulty) {
-                    difficulty = maxDifficulty;
-                    difficultyInput.text = difficulty.ToString();
+        if(hasInputedDifficulty == false) {
+            if(difficultyInput.text != null && Input.GetKeyDown(KeyCode.Return)) {
+                if(int.TryParse(difficultyInput.text, out difficulty)) {
+                    difficulty = int.Parse(difficultyInput.text);
+                    difficulty = Mathf.Clamp(difficulty, 1, 10);
+                }else {
+                    difficultyInput.text = "Please Input A Number";
                 }
-            }else {
-                difficultyInput.text = "Please Input A Valid Number";
             }
         }
 
-        if(currentHealth <= 0) {
-            //Triggers when player dies from less than or 0 currentHealth
-            Die();
-        }
-    }
+        if(hasInputedDifficulty == true && hasInputedName == true) {
+            hasPlayedInput.gameObject.SetActive(true);
+            if(Input.GetKeyDown(KeyCode.Return)) {
+                for (int i = 0; i < acceptedHasPlayedStringsTrue.Length; i++) {
+                    if(hasPlayedInput.text == acceptedHasPlayedStringsTrue[i]) {
+                        hasPlayed = true;
+                        StartGameWithoutTutorial();
+                    }
+                }
 
-    private void Die() {
-        //Triggers when player dies from less than or 0 currentHealth
-    }
+                for (int i = 0; i < acceptedHasPlayedStringsFalse.Length; i++) {
+                    if(hasPlayedInput.text == acceptedHasPlayedStringsFalse[i]) {
+                        hasPlayed = false;
+                        StartTutorial();
+                    }
+                }
 
-    public void AddAttack(int amount) {
-        if(statsLeft > 0) {
-            if(amount < 0 && statsLeft > 0){ 
-
-            }else {
-                attack += amount;
-                attack = Mathf.Clamp(attack, 0, maxStats);
-                attackText.text = attack.ToString();
+                for (int i = 0; i < wierdInputStrings.Length; i++) {
+                    if(hasPlayedInput.text == wierdInputStrings[i]) {
+                        hasPlayed = false;
+                        hasPlayedInput.text = hasPlayedInput + "?" + " Are You Stupid?, Fuck Off!";
+                    }
+                }
             }
+        }else {
+            hasPlayedInput.gameObject.SetActive(false);
         }
     }
-    public void AddStamina(int amount) {
-        stamina += amount;
-        stamina = Mathf.Clamp(stamina, 0, maxStats);
-        staminaText.text = stamina.ToString();
-    }
-    public void AddDefence(int amount) {
-        defence += amount;
-        defence = Mathf.Clamp(defence, 0, maxStats);
-        defenceText.text = defence.ToString();
+
+    //Button Changes Attack Variable
+    public void ChangeAttack(int change) {
+        attack += change;
     }
 
+    //Button Changes Health Variable
+    public void ChangeHealth(int change) {
+        health += change;
+    }
+
+    //Button Changes Defence Varibale
+    public void ChangeDefence(int change) {
+        defence += change;
+    }
+
+    //Starts Game Without Tutorial
+    public void StartGameWithoutTutorial() {
+
+    }
+
+    //Starts Game With Tutorial
+    public void StartTutorial() {
+
+    }
 }
