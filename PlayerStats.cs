b73e4710b1698365a1e4b69372cc38f5344aa8bc
CupOJoe-hub <66897604+CupOJoe-hub@users.noreply.github.com>
2021-07-30 14:56:03 +1200

    Update PlayerStats.cs

--- PlayerStats.cs
+++ PlayerStats.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 
 //This Script Collects Data From The Player Such As Player Name, Game Difficulty, And Character Customisation
-public class PlayerStats01 : MonoBehaviour
+public class PlayerStats : MonoBehaviour
 {
     //Variables That Store And Collect Name Input
     public string name;
