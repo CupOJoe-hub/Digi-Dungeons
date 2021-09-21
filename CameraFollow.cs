bf31ef034357fb0a4443573779ede37cb1f0d6e8
CupOJoe-hub <66897604+CupOJoe-hub@users.noreply.github.com>
2021-09-22 10:05:41 +1200

    Add files via upload

---
+++ CameraFollow.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform followTarget;
+    public Transform camera;
+    public Vector3 offset;
+    public float smoothTime;
+
+    private void Update() {
+        Vector3 finalPos = followTarget.position + offset;
+
+        camera.position = Vector3.Lerp(camera.position, finalPos, smoothTime);
+    }
+}
