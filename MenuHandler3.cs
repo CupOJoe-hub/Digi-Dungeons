da743b2719c9936842ff040ef4e5134105fa9814
CupOJoe-hub <66897604+CupOJoe-hub@users.noreply.github.com>
2021-09-23 10:35:05 +1200

    Add files via upload

---
+++ MenuHandler3.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuHandler : MonoBehaviour
+{
+    public GameObject optionsPanel;
+    public bool isOpen;
+
+    private void Start() {
+        optionsPanel.SetActive(false);
+        isOpen = false;
+    }
+
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.Escape)) {
+            isOpen = !isOpen;
+            optionsPanel.SetActive(isOpen);
+            if(optionsPanel.activeSelf == true) {
+                Time.timeScale = 0f;
+            }else {
+                Time.timeScale = 1f;
+            }
+        }
+    }
+
+    public void Resume() {
+        optionsPanel.SetActive(false);
+        isOpen = false;
+        Time.timeScale = 1f;
+    }
+
+    public void Exit() {
+        SceneManager.LoadScene("Menu");
+    }
+}
